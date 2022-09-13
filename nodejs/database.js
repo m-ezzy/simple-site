44f216d7822d5e2c2b54225c1c5636121cf7ee53
murtaza <murtazaezzy1001@gmail.com>
2022-09-13 17:53:09 +0530

    8

--- nodejs/database.js
+++ nodejs/database.js
@@ -10,19 +10,23 @@ var con = mysql.createConnection({
 });*/
 
 var con = mysql.createConnection({
-	//host: "skyland-362206:us-central1:mysql-db-1",
-	host: "35.184.85.39",
+	host: "skyland-362206:us-central1:mysql-db-1",
+	//host: "35.184.85.39",
 	//host: "https://skyland-362206.el.r.appspot.com/",
-	user: "root",
-	password: "]uel{GT:&#&E~f}d",
+	/*user: "root",
+	password: "]uel{GT:&#&E~f}d",*/
+	user: "admin1",
+	password: "r^hx81{F-%75&Naa",
 	database: "simple-site",
 });
 
 con.query = util.promisify(con.query).bind(con);
 
-con.connect(function(err) {
+con.connect();
+/*
+con.connect((err) => {
 	if (err) throw err;
 	console.log("mysql database connected!");
-});
+});*/
 
 exports.con = con;
